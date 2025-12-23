1fc1add588cf5fd6de30ca4f84a8add1f15e0a86
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-12-24 04:24:36 +0530

    added files

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map('i', '<S-Tab>', function ()
+  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+end, { desc = 'Copilot Accept', noremap = true, silent = true })
---
+++ .config/nvim/pack/github/start/copilot.vim/autoload/copilot/version.vim
@@ -0,0 +1,3 @@
+function! copilot#version#String() abort
+  return '1.57.0'
+endfunction
