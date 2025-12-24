d1ba276876d3587dd04e4105c30bd4728a6c46c7
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-12-24 16:13:49 +0530

    tmux config completed

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,10 +4,19 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- basic remaps
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Copilot Accept
 map('i', '<S-Tab>', function ()
   vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
 end, { desc = 'Copilot Accept', noremap = true, silent = true })
+
+-- Tmux navigator mappings (from image)
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>",  { desc = "window left" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "window right" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>",  { desc = "window down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>",    { desc = "window up" })
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -13,14 +13,20 @@ return {
     end,
   },
 
-{
+  {
     "github/copilot.vim",
     lazy = false,
     config = function() -- Mapping tab is already used in NvChad
       vim.g.copilot_no_tab_map = true; -- Disable tab mapping
       vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
-    end
+    end,
+  },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
   }
+
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
